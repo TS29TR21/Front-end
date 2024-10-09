d24035f297b747f45e3064ee2a22b8489040b24e
TS29TR21 <169053192+TS29TR21@users.noreply.github.com>
2024-10-09 13:48:09 +0200

    Completed routing pages

--- frontend-bananaboat/src/App.js
+++ frontend-bananaboat/src/App.js
@@ -3,6 +3,14 @@ import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./components/home.js";
 import Login from "./components/login.js";
+import ResourceSearch from "./components/resource-report.js";
+import ModerationForm from "./components/moderation.js";
+import ChangePassword from "./components/new-password.js";
+import RateResource from "./components/rate-resource.js";
+import Register from "./components/register.js";
+import PasswordReset from "./components/reset-password.js";
+import ResourceReport from "./components/resource-report.js";
+import UpdateUserRole from "./components/update-user-role.js";
 
 function App() {
   return (
@@ -10,8 +18,16 @@ function App() {
       <Router>
         <div>
           <Routes>
-            <Route path="/home-page" element={<Home />} />
+            <Route path="/" element={<Home />} />
             <Route path="/login-page" element={<Login />} />
+            <Route path="/resource-search-page" element={<ResourceSearch />} />
+            <Route path="/moderate-page" element={<ModerationForm />} />
+            <Route path="/new-password-page" element={<ChangePassword />} />
+            <Route path="/rate-resource-page" element={<RateResource />} />
+            <Route path="/register-page" element={<Register />} />
+            <Route path="/reset-password-page" element={<PasswordReset />} />
+            <Route path="/resource-report-page" element={<ResourceReport />} />
+            <Route path="/update-role-page" element={<UpdateUserRole />} />
           </Routes>
         </div>
       </Router>
