20ffcb474055c37147b5957d6c5308521e435436
TS29TR21 <169053192+TS29TR21@users.noreply.github.com>
2024-10-15 14:55:24 +0200

    Bootstrap icons

--- frontend-bananaboat/npm-modules.bat
+++ frontend-bananaboat/npm-modules.bat
@@ -3,4 +3,6 @@ npm install serve --save
 npm install react-router-dom
 npm install @mui/material @emotion/react @emotion/styled @mui/icons-material
 npm install react-bootstrap bootstrap
+npm install @popperjs/core
+npm i bootstrap-icons
 
